5c225ef6008a3b638f119637091d79441ca9fe89
isaacleal10 <elpisaac38@gmail.com>
2022-11-28 09:03:48 -0500

    cambios

--- cal_salari/asgi.py
+++ cal_salari/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for actividad_final project.
+ASGI config for cal_salari project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'actividad_final.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cal_salari.settings')
 
 application = get_asgi_application()
--- cal_salari/wsgi.py
+++ cal_salari/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for actividad_final project.
+WSGI config for cal_salari project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'actividad_final.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cal_salari.settings')
 
 application = get_wsgi_application()
